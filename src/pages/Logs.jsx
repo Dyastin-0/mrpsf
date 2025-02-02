64853af1b7b12596c5b65439b5ee079c73c0da5d
Justine <paralejasjustine15@gmail.com>
2025-02-03 05:16:21 +0800

    Small fix

--- src/pages/Logs.jsx
+++ src/pages/Logs.jsx
@@ -1,5 +1,3 @@
-import clsx from "clsx";
-import Proxy from "../components/Proxy";
 import useLogs from "../hooks/useLogs";
 import Log from "../components/Log";
 
@@ -11,7 +9,7 @@ const Logs = () => {
       className="flex w-[calc(100%-.75rem)] h-[calc(100%-.75rem)] justify-center bg-primary text-primary-foreground rounded-md p-3
       mr-3 mb-3 overflow-y-auto font-mono"
     >
-      <div className="flex flex-col w-full h-fit">
+      <div className="flex flex-col w-full h-full gap-3">
         {logs.length > 0 &&
           logs.map((log, index) => <Log log={log} key={index} />)}
       </div>
