7200c7a5ce2928e8691b337b0f0ea42e95299360
Justine <paralejasjustine15@gmail.com>
2025-02-03 13:46:16 +0800

    Small fix

--- src/components/Logs.jsx
+++ src/components/Logs.jsx
@@ -7,7 +7,7 @@ const Logs = () => {
   return (
     <div
       className="flex flex-col w-[700px] max-w-fit max-h-[450px] overflow-y-auto font-mono gap-3
-    scrollbar-thin scrollbar-track-transparent scrollbar-thumb-primary-highlight"
+    custom-scrollbar"
     >
       {logs.length > 0 &&
         logs.map((log, index) => <Log log={log} key={index} />)}
