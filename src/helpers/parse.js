dcb530faeef6bfa5de955e4a42666e0b054a902c
Justine <paralejasjustine15@gmail.com>
2025-02-03 14:19:36 +0800

    Small fix

--- src/helpers/parse.js
+++ src/helpers/parse.js
@@ -2,7 +2,8 @@ export const parseJSON = (log) => {
   try {
     return JSON.parse(log);
   } catch (error) {
-    console.error("Invalid JSON log:", log);
+    console.log(log);
+    console.error("Invalid JSON log:", error);
     return { error: "Invalid log format" };
   }
 };
