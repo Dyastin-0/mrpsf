36d46addc801172dd8d919988c8a1931fe28d6fc
Justine <paralejasjustine15@gmail.com>
2025-02-03 15:32:47 +0800

    Color

--- src/components/ui/Dot.jsx
+++ src/components/ui/Dot.jsx
@@ -1,5 +1,9 @@
 export const Dot = ({ value }) => {
   return (
-    <span className={`rounded-full w-2 h-2 ${value ? "bg-green" : "bg-red"}`} />
+    <span
+      className={`rounded-full w-2 h-2 ${
+        value ? "bg-green-500" : "bg-red-600"
+      }`}
+    />
   );
 };
